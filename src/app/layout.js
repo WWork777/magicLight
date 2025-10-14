b18530c1384531201dafdba1d0e7827e3aec4628
unknown <parsikovevgenij470@gmail.com>
2025-10-14 21:49:54 +0700

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,27 +1,27 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import { Montserrat, Wix_Madefor_Display } from 'next/font/google';
-import Header from '@/components/layout/header/Header';
-import Footer from '@/components/layout/footer/Footer';
-import './globals.css';
-import Script from 'next/script';
-import YClientsInit from '@/components/common/YClientsInit/YClientsInit';
-import CookieBanner from '@/components/common/CookieBanner/CookieBanner';
-import YandexMetrika from '@/components/common/YandexMetrika/YandexMetrika';
+import { useState, useEffect } from "react";
+import { Montserrat, Wix_Madefor_Display } from "next/font/google";
+import Header from "@/components/layout/header/Header";
+import Footer from "@/components/layout/footer/Footer";
+import "./globals.css";
+import Script from "next/script";
+import YClientsInit from "@/components/common/YClientsInit/YClientsInit";
+import CookieBanner from "@/components/common/CookieBanner/CookieBanner";
+import YandexMetrika from "@/components/common/YandexMetrika/YandexMEtrika";
 
 const montserrat = Montserrat({
-  subsets: ['latin', 'cyrillic'],
-  weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-montserrat',
-  display: 'swap',
+  subsets: ["latin", "cyrillic"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-montserrat",
+  display: "swap",
 });
 
 const wixMadefor = Wix_Madefor_Display({
-  subsets: ['latin', 'cyrillic'],
-  weight: ['400', '500', '700'],
-  variable: '--font-wix',
-  display: 'swap',
+  subsets: ["latin", "cyrillic"],
+  weight: ["400", "500", "700"],
+  variable: "--font-wix",
+  display: "swap",
 });
 
 export default function RootLayout({ children }) {
@@ -29,21 +29,21 @@ export default function RootLayout({ children }) {
   const [consentGiven, setConsentGiven] = useState(false);
 
   useEffect(() => {
-    const consent = localStorage.getItem('cookieConsent');
-    if (consent === 'accepted') {
+    const consent = localStorage.getItem("cookieConsent");
+    if (consent === "accepted") {
       setConsentGiven(true);
     }
   }, []);
   // ⬆️ ДО return
 
   return (
-    <html lang='ru' className={`${montserrat.variable} ${wixMadefor.variable}`}>
+    <html lang="ru" className={`${montserrat.variable} ${wixMadefor.variable}`}>
       <head>
         {/* YCLIENTS скрипт */}
         <Script
-          src='https://w745741.yclients.com/widgetJS'
-          strategy='afterInteractive'
-          charSet='UTF-8'
+          src="https://w745741.yclients.com/widgetJS"
+          strategy="afterInteractive"
+          charSet="UTF-8"
         />
       </head>
 
