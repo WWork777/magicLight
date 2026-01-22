03bcbb87a931c804fb44a1e9b95b80431cf9426f
id0ntc4re <parsikovevgenij470@gmail.com>
2026-01-22 13:34:07 +0700

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -166,18 +166,6 @@ export default function HomePage() {
       <Studio />
       <Reviews />
       <Contacts />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-(function(w, d, s, h, id) {
-    w.roistatProjectId = id; w.roistatHost = h;
-    var p = d.location.protocol == "https:" ? "https://" : "http://";
-    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);
-    var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
-})(window, document, 'script', 'cloud.roistat.com', 'c10ac2a4580e1360e22ec21a1db03398');
-            `,
-        }}
-      />
     </>
   );
 }
